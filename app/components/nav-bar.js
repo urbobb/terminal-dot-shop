87e072c78f8e1778ffc04234629fae0638c82a71
Bangerr <devilism09@gmail.com>
2025-05-10 01:19:37 +0200

    implement logout

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,6 +1,12 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class NavBarComponent extends Component {
   @service session;
+
+  @action
+  invalidateSession() {
+    this.session.invalidate();
+  }
 }
