297d985ce975e69337dbf6ae27a323a8af236ea4
Bangerr <devilism09@gmail.com>
2025-05-25 20:44:36 +0200

    fix: update address handling in profile controller to support multiple addresses

--- app/controllers/profile.ts
+++ app/controllers/profile.ts
@@ -1,20 +1,11 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 import type { Profile, Address } from 'terminal-dot-shop/types/terminal-api';
 
 interface ProfileRouteModel {
   profile: Profile | null;
-  address: Address | null;
+  addresses: Address[] | null;
 }
-
-interface UserAddress {
-  street: string;
-  city: string;
-  postalCode: string;
-  state: string;
-  country: string;
-  phone: string | null;
-}
-
 export default class ProfileController extends Controller {
   model: ProfileRouteModel | null = null;
 
@@ -36,17 +27,10 @@ export default class ProfileController extends Controller {
     return '';
   }
 
-  get userAddress(): UserAddress | undefined {
-    const address = this.model?.address;
-    if (address) {
-      return {
-        street: address[0].street1,
-        city: address[0].city,
-        postalCode: address[0].zip,
-        state: address[0].state,
-        country: address[0].country,
-        phone: address[0].phone ? address[0].phone : null,
-      };
+  get userAddress(): Address[] | undefined {
+    const addresses = this.model?.addresses;
+    if (addresses && addresses.length > 0) {
+      return addresses;
     }
     return undefined;
   }
