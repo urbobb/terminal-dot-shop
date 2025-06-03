d1af0cd97c9a5e88e4ae5ad3f1ca82e55b51930e
Bangerr <devilism09@gmail.com>
2025-06-03 23:48:27 +0200

    refactor: declare service types for session and product in route and service classes

--- app/routes/coffees.ts
+++ app/routes/coffees.ts
@@ -1,9 +1,11 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import type ProductService from 'terminal-dot-shop/services/product';
+import type CustomSessionService from 'terminal-dot-shop/services/session';
 
 export default class CoffeesRoute extends Route {
-  @service session;
-  @service product;
+  @service declare session: CustomSessionService;
+  @service declare product: ProductService;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
--- app/services/product.ts
+++ app/services/product.ts
@@ -1,9 +1,9 @@
 import Service from '@ember/service';
 import { service } from '@ember/service';
 import Terminal from '@terminaldotshop/sdk';
-
+import type CustomSessionService from './session';
 export default class ProductService extends Service {
-  @service session: any;
+  @service declare session: CustomSessionService;
 
   async getProducts() {
     const access_token = this.session.data.authenticated.access_token as string;
